86e5d6123e62a0a04030d3083d432f6b1ebf55a3
Tania Tasmin <taniat.kaau@gmail.com>
2021-10-26 00:13:20 +0000

    Modify the tail.js file to check the equality of 2 arrays

--- tail.js
+++ tail.js
@@ -1,5 +1,12 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  let b = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i])     {
+      b = false;
+      break;
+    }
+  }
+  if (b === true) {
     console.log(`✔✔✔Assertion Passed:${actual} === ${expected}`);
   } else {
     console.log(`🥵😱🥵😱Assertion Failed:${actual} !== ${expected}`);
@@ -21,7 +28,13 @@ tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 // Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+let  result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+// Test Case 2: Check the returned array elements
+result = tail("One Element");
+assertEqual(result, []);
+
+// Test Case 3: Check the returned array elements
+result = tail();
+assertEqual(result, []);
