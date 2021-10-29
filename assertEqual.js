a90534b553b2f587421253bd4547297fe0205ba1
Tania Tasmin <taniat.kaau@email.com>
2021-10-29 06:32:29 -0400

    Modify function assertEqual slightly to change Emojis

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔✔✔Assertion Passed:${actual} === ${expected}`);
   } else {
-    console.log(`🥵😱🥵😱Assertion Failed:${actual} !== ${expected}`);
+    console.log(`❌❌❌Assertion Failed:${actual} !== ${expected}`);
   }
 };
 
