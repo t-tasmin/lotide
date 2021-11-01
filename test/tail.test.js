2cdecac6335a31f2501f7546ddf2b48e7e9192d1
Tania Tasmin <taniat.kaau@email.com>
2021-11-01 16:05:09 -0400

    Modify the test file tail.test to include mocha and chai functions

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,13 +1,30 @@
-const tail= require('../tail');
-const assertArraysEqual= require('../assertArraysEqual');
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-let  A = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(A, ["Lighthouse", "Labs"]);
+describe("#tail",()=>{
 
-// Test Case 2: Check the returned array elements
-A = tail("One Element");
-assertArraysEqual(A, []);
+  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    const expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(result, expected);
+  });
 
-// Test Case 3: Check the returned array elements
-A = tail();
-assertArraysEqual(A, []);
+  it('should return [2,3,4] for [1,2,3,4]', () => {
+    const result = tail([1,2,3,4]);
+    const expected = [2,3,4];
+    assert.deepEqual(result, expected);
+  });
+
+  it('should return [ ] for ["One element"]', () => {
+    const result = tail(["One element"]);
+    const expected = [];
+    assert.deepEqual(result, expected);
+  });
+
+  it('should return [ ] for undefined input', () => {
+    const result = tail();
+    const expected = [];
+    assert.deepEqual(result, expected);
+  });
+});
+
