db129bf5be1a4ca82678fdc67b373596e6a765b0
Tania Tasmin <taniat.kaau@email.com>
2021-11-01 13:58:19 -0400

    Modify function tail to import eqArrays and assertArraysEqual functions and to export tail function

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let b = true;
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i])     {
-      b = false;
-      break;
-    }
-  }
-  if (b === true) {
-    console.log(`✔✔✔Assertion Passed:${actual} === ${expected}`);
-  } else {
-    console.log(`🥵😱🥵😱Assertion Failed:${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const tail = function(A) {
@@ -22,19 +10,4 @@ const tail = function(A) {
   }
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 1: Check the returned array elements
-let  result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-// Test Case 2: Check the returned array elements
-result = tail("One Element");
-assertEqual(result, []);
-
-// Test Case 3: Check the returned array elements
-result = tail();
-assertEqual(result, []);
+module.exports = tail; 
