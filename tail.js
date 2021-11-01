776ff7eed2c50469f9fc34f86f720462f16632ce
Tania Tasmin <taniat.kaau@email.com>
2021-11-01 14:45:36 -0400

    Modify function tail (it does not need to import anything)

--- tail.js
+++ tail.js
@@ -1,7 +1,3 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
-
 const tail = function(A) {
   if (Array.isArray(A))  {
     return A.slice(1);
