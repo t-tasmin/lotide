bb0b9fcd789adf5a0d9bb162b154baaed811fa63
Tania Tasmin <taniat.kaau@email.com>
2021-11-02 07:33:40 -0400

    Create a test function to test letterPositions using mocha and chai

---
+++ test/letterPositions.test.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const letterPositions = require('../letterPositions');
+
+describe("#countLetters",()=>{
+
+  it('Test Case 1"', () => {
+    const result = letterPositions("hello molly").l;
+    const expected = [2,3,8,9];
+    assert.deepEqual(result, expected);
+  });
+
+});
